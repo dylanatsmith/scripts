16c2681daf1cd7d62dacea42b5692d5a8fa609b8
Dylan Smith <dylanatsmith@gmail.com>
2019-02-21 21:15:08 +0000

    Added more lines to check because of varying HTML structure on page

--- pedal-sniper/pedal-sniper.sh
+++ pedal-sniper/pedal-sniper.sh
@@ -11,19 +11,31 @@ echo "$(curl -o "$newFile" "$url")"
 # From the original file, grab the two possible lines the first product name can appear on
 oldProductA="$(tail -n+2418 "$originalFile" | head -n1)"
 oldProductB="$(tail -n+2429 "$originalFile" | head -n1)"
+oldProductC="$(tail -n+2465 "$originalFile" | head -n1)" # Two later lines account for possibility of pagination element
+oldProductD="$(tail -n+2476 "$originalFile" | head -n1)"
 
 # From the new file, grab the two possible lines the first product name can appear on
 newProductA="$(tail -n+2418 "$newFile" | head -n1)"
 newProductB="$(tail -n+2429 "$newFile" | head -n1)"
+newProductC="$(tail -n+2465 "$newFile" | head -n1)"
+newProductD="$(tail -n+2476 "$newFile" | head -n1)"
 
 # Print those lines from the files
 echo "$oldProductA"
 echo "$oldProductB"
+echo "$oldProductC"
+echo "$oldProductD"
 echo "$newProductA"
 echo "$newProductB"
+echo "$newProductC"
+echo "$newProductD"
 
 # Check if latest product title in the old file matches the new one
-if [ "$newProductA" == "$oldProductA" ] || [ "$newProductA" == "$oldProductB" ] || [ "$newProductB" == "$oldProductA" ] || [ "$newProductB" == "$oldProductB" ]; then
+# if [ "$newProductA" == "$oldProductA" ] || [ "$newProductA" == "$oldProductB" ] || [ "$newProductB" == "$oldProductA" ] || [ "$newProductB" == "$oldProductB" ]; then
+if [[ "$newProductA" =~ ^("$oldProductA"|"$oldProductB"|"$oldProductC"|"$oldProductD")$ ]] ||
+	 [[ "$newProductB" =~ ^("$oldProductA"|"$oldProductB"|"$oldProductC"|"$oldProductD")$ ]] ||
+	 [[ "$newProductC" =~ ^("$oldProductA"|"$oldProductB"|"$oldProductC"|"$oldProductD")$ ]] ||
+	 [[ "$newProductD" =~ ^("$oldProductA"|"$oldProductB"|"$oldProductC"|"$oldProductD")$ ]]; then
 	printf "\n\n Nothing new this time \n\n\n"
 else
 	printf "\n\n NEW PEDAL ALERT \n\n\n"
